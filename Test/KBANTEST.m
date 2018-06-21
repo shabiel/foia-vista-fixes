a88ab5cda09cf896cec0d1a575daa8cbae1b899d
Sam Habiel <Sam.Habiel@gmail.com>
2018-06-21 15:58:47 -0400

    Completed ICDEXA2 and MPIFXMLP

---
+++ Test/KBANTEST.m
@@ -0,0 +1,21 @@
+KBANTEST ; OSE/SMH - Unit Tests for Minor OSEHRA fixes to FOIA;2018-06-21  3:54 PM
+ ;;0.0;No package;
+ ;
+ D EN^%ut($T(+0),3)
+ QUIT
+ ;
+MPIFXMLP ; @TEST PATIENT^MPIFXMLP does not exist
+ D CHKTF^%ut($T(PATIENT^MPIFXMLP)="")
+ QUIT
+ ;
+ICDEXA2 ; @TEST ICDEXA2 will work w/o converting ICDO to numbers
+ D CHKEQ^%ut($$NEXT^ICDEXA2("",30,DT),"A00.0")
+ D CHKEQ^%ut($$NEXT^ICDEXA2("S31.603S",30,DT),"S31.604A")
+ D CHKEQ^%ut($$NEXT^ICDEXA2("",30),"A00.0")
+ D CHKEQ^%ut($$NEXT^ICDEXA2("S31.603S",30),"S31.604A")
+ ;
+ D CHKEQ^%ut($$PREV^ICDEXA2("",30,DT),"Z99.89")
+ D CHKEQ^%ut($$PREV^ICDEXA2("Z99.89",30,DT),"Z99.81")
+ D CHKEQ^%ut($$PREV^ICDEXA2("",30),"Z99.89")
+ D CHKEQ^%ut($$PREV^ICDEXA2("Z99.89",30),"Z99.81")
+ QUIT
