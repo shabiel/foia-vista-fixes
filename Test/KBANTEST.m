320ec46437abd6ddfa2ff52caa4284ae1c581c46
Sam Habiel <Sam.Habiel@gmail.com>
2018-06-21 16:42:50 -0400

    Fix to GET1^ORWORR1

--- Test/KBANTEST.m
+++ Test/KBANTEST.m
@@ -1,4 +1,4 @@
-KBANTEST ; OSE/SMH - Unit Tests for Minor OSEHRA fixes to FOIA;2018-06-21  3:54 PM
+KBANTEST ; OSE/SMH - Unit Tests for Minor OSEHRA fixes to FOIA;2018-06-21  4:29 PM
  ;;0.0;No package;
  ;
  D EN^%ut($T(+0),3)
@@ -18,4 +18,17 @@ ICDEXA2 ; @TEST ICDEXA2 will work w/o converting ICDO to numbers
  D CHKEQ^%ut($$PREV^ICDEXA2("Z99.89",30,DT),"Z99.81")
  D CHKEQ^%ut($$PREV^ICDEXA2("",30),"Z99.89")
  D CHKEQ^%ut($$PREV^ICDEXA2("Z99.89",30),"Z99.81")
+ QUIT
+ ;
+ORWORR1 ; @TEST GET1^ORWORR1 contains && that crashes GTM
+ N ORLIST S ORLIST="KBANLIST"
+ N MULT S MULT=1
+ N FILTER S FILTER=1
+ N ORWTS S ORWTS=0
+ N EVTNAME S EVTNAME=""
+ K ^TMP("ORR",$J,ORLIST)
+ S ^TMP("ORR",$J,ORLIST,1)=1
+ S ^TMP("ORR",$J,ORLIST,"TOT")=1
+ D GET1^ORWORR1
+ D SUCCEED^%ut
  QUIT
