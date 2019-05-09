ebb29b4d8bfd5f5d744adfe2f85c7d5a8f21fe41
Sam Habiel <Sam.Habiel@gmail.com>
2019-05-09 11:11:07 -0400

    Update MPIFXMLP with patch MPIF*1*67

--- Routines/MPIFXMLP.m
+++ Routines/MPIFXMLP.m
@@ -1,5 +1,5 @@
-MPIFXMLP ;OAK/ELZ - MPIF PROBLISTIC SEARCH ;2018-06-21  2:56 PM
- ;;1.0;MASTER PATIENT INDEX VISTA;**61,OSEHRA**;30 Apr 99;Build 3
+MPIFXMLP ;OAK/ELZ - MPIF PROBLISTIC SEARCH ;2019-05-09  11:10 AM
+ ;;1.0;MASTER PATIENT INDEX VISTA;**61,67,OSEHRA**;30 Apr 99;Build 3
  ;
  ; *OSEHRA changes by Sam Habiel (c) 2018.
  ; To test: DG REGISTER PATIENT; there should be no MPI lookup.
@@ -16,7 +16,8 @@ ZPATIENT(RETURN,MPIARR) ; - query for patients based on traits *OSEHRA rename PA
  D PARSE(.RETURN,.MPIXMLR)
  ;
  ; convert dob to fm format
- S MPIPAT=0 F  S MPIPAT=$O(RETURN(MPIPAT)) Q:'MPIPAT  I $D(RETURN(MPIPAT,"DOB")) S RETURN(MPIPAT,"DOB")=$$HL7TFM^XLFDT(RETURN(MPIPAT,"DOB"))
+ ; Story 722746 need DOD formatted as well if there is one
+ S MPIPAT=0 F  S MPIPAT=$O(RETURN(MPIPAT)) Q:'MPIPAT  S:$D(RETURN(MPIPAT,"DOD")) RETURN(MPIPAT,"DOD")=$$HL7TFM^XLFDT(RETURN(MPIPAT,"DOD")) I $D(RETURN(MPIPAT,"DOB")) S RETURN(MPIPAT,"DOB")=$$HL7TFM^XLFDT(RETURN(MPIPAT,"DOB"))
  ;
  ;
  Q
@@ -218,6 +219,8 @@ SE(MPIN,MPIA) ; - used for the parser to call back with STARTELEMENT
  . I MPIA("type")="MMN" S MPIUSE="MMN" Q
  . I MPIA("type")="DOB" S MPIUSE="DOB" Q
  . I MPIA("type")="GENDER" S MPIUSE="Gender" Q
+ . ; Story 722746 (elz) need DOD if there is one
+ . I MPIA("type")="DEATHDATE" S MPIUSE="DOD" Q
  I MPIN="VALUE" D  K MPIUSE Q
  . I $L(MPIUSE) S MPIVAR=","""_MPIUSE_""")"
  I MPIN="ADDRESS" D  Q
